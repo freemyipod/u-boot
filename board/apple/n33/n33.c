556d31eedeb2e008925fbf32cdc29ec44bf4979a
Vencislav Atanasov <git@6bez10.info>
2025-06-21 23:38:50 +0200

    Unify N33 and N31 configs, always initialize N33 board, not just when using debug UART

--- board/apple/n33/n33.c
+++ board/apple/n33/n33.c
@@ -39,34 +39,43 @@ int print_cpuinfo(void)
     return 0;
 }
 
-#ifdef CONFIG_BOARD_EARLY_INIT_F
-int board_early_init_f(void)
-{
-    debug("board_early_init_f\n");
+void board_gpio_init(void) {
+    // Enable GPIO pins on N5G.
+    static volatile uint32_t *gpio = (uint32_t *)S5L87XX_GPIOBASE_ADDR;
+    *gpio &= 0xff00ffff;
+    *gpio |= 0x00220000;
+}
+
+void board_clock_init(void) {
+    s5l87xx_enable_clkgate("uart0");
+
     // HACKHACKHACK add a pmctrl to linux
     // needed for timer c0..???
     s5l87xx_enable_clkgate("timer3");
     // HACKHACKHACK
+}
 
+void board_vic_init(void) {
     // Disable all VIC interrupts.
     // TODO(q3k): disable VIC elsewhere
     static volatile uint32_t *vic0_enclr = (uint32_t *)(S5L87XX_VICBASE_ADDR + 0x14);
     static volatile uint32_t *vic1_enclr = (uint32_t *)(S5L87XX_VICBASE_ADDR + 0x14 + 0x1000);
     *vic0_enclr = 0xffffffff;
     *vic1_enclr = 0xffffffff;
+}
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+int board_early_init_f(void)
+{
+    /* configuring UART TX & RX line GPIO */
+    board_gpio_init();
+
+    /* enabling UART clocking */
+    board_clock_init();
+    
+    /* configure VIC */
+    board_vic_init();
 
     return 0;
-}
-#endif
-
-#ifdef CONFIG_DEBUG_UART_BOARD_INIT
-void board_debug_uart_init(void)
-{
-    s5l87xx_enable_clkgate("uart0");
-
-    // Enable GPIO pins on N5G.
-    static volatile uint32_t *gpio = (uint32_t *)S5L87XX_GPIOBASE_ADDR;
-    *gpio &= 0xff00ffff;
-    *gpio |= 0x00220000;
 }
 #endif
