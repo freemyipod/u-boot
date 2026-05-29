f3b0673b43ec62ef604939974c1acbdd24ebd3c1
Vencislav Atanasov <user890104@freemyipod.org>
2026-05-30 02:42:59 +0300

    N46: unify config, fix load address and size

--- include/configs/apple_n46.h
+++ include/configs/apple_n46.h
@@ -6,7 +6,6 @@
 // Just guessing.
 #define CFG_SYS_INIT_RAM_SIZE	0x00030000
 
-#define CFG_EXTRA_ENV_SETTINGS \
-	"dfu_alt_info=kernel ram 0x08000000 0x01000000\0"
+#define CFG_EXTRA_ENV_SETTINGS "dfu_alt_info=kernel ram 0x09000000 0x00800000\0"
 
 #endif	/* __CONFIG_H */
