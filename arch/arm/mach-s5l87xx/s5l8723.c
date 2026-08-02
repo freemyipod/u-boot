a58226520da07b83377a67aee94cd8998c549e06
Vencislav Atanasov <user890104@freemyipod.org>
2026-08-02 13:40:39 +0300

    S5L8723/N20: Add support for new SoC and board: iPod nano (6th generation)

---
+++ arch/arm/mach-s5l87xx/s5l8723.c
@@ -0,0 +1,19 @@
+#include <init.h>
+
+#if defined(CONFIG_DISPLAY_CPUINFO)
+int print_cpuinfo(void) {
+    printf("CPU:   Samsung S5L8723 (ARM1176JZF-S)\n");
+    return 0;
+}
+#endif
+
+void lowlevel_init(void)
+{
+    // unused
+}
+
+ulong get_tbclk(void)
+{
+    // Corresponds to TIMER_F setup from s5l87xx.c.
+    return 1000000;
+}
---
+++ board/apple/n20/n20.c
@@ -0,0 +1,69 @@
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <asm/arch-s5l87xx/s5l87xx.h>
+#include <asm/arch-s5l87xx/s5l87xx-clk.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+    return fdtdec_setup_mem_size_base();
+}
+
+int dram_init_banksize(void)
+{
+    return fdtdec_setup_memory_banksize();
+}
+
+int board_init(void)
+{
+    return 0;
+}
+
+static void board_gpio_init(void) {
+    // UART0
+    uint32_t gpio = readl(S5L87XX_PCON(0));
+    gpio &= S5L87XX_PCON_PINS_CLEAR_FN(4, 5);
+    gpio |= S5L87XX_PCON_PINS_SET_FN(4, 5, 0x2);
+    writel(gpio, S5L87XX_PCON(0));
+}
+
+static void board_clock_init(void) {
+    s5l87xx_enable_clkgate("uart0");
+}
+
+static void board_vic_init(void) {
+    // Disable all VIC interrupts.
+    // TODO(q3k): disable VIC elsewhere
+    writel(0xffffffff, S5L87XX_VICINTENCLEAR(0));
+    writel(0xffffffff, S5L87XX_VICINTENCLEAR(1));
+}
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+int board_early_init_f(void)
+{
+    /* Enable GPIO pins */
+    board_gpio_init();
+
+    /* Enable clocking */
+    board_clock_init();
+    
+    /* Configure VIC */
+    board_vic_init();
+
+    return 0;
+}
+#endif
+
+#ifdef CONFIG_DEBUG_UART_BOARD_INIT
+void board_debug_uart_init(void)
+{
+    /* Enable GPIO pins */
+    board_gpio_init();
+    
+    /* Enable UART clocking. Runs before the FDT is set up, so ungate uart0 by
+     * raw {gate, bit} ({1,9} + {4,7}) rather than the device-tree name lookup. */
+    s5l87xx_enable_clkgate_bit(1, 9);
+    s5l87xx_enable_clkgate_bit(4, 7);
+}
+#endif
