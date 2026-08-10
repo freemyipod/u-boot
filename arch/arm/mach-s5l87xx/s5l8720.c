66f2ea74b647f14f0cc0aaa793d888c1f03ddd8f
Vencislav Atanasov <user890104@freemyipod.org>
2026-08-10 20:02:21 +0300

    S5L8720/N58: Add support for new SoC and board: iPod nano (4th generation)

---
+++ arch/arm/mach-s5l87xx/s5l8720.c
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2026 The freemyipod team (freemyipod.org)
+ */
+
+#include <init.h>
+
+#if defined(CONFIG_DISPLAY_CPUINFO)
+int print_cpuinfo(void) {
+    printf("CPU:   Samsung S5L8720 (ARM1176JZF-S)\n");
+    return 0;
+}
+#endif
+
+void lowlevel_init(void)
+{
+    // unused
+}
+
+ulong get_tbclk(void)
+{
+    // Corresponds to TIMER_F setup from s5l87xx.c.
+    return 1000000;
+}
