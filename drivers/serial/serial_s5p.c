194ba6a794942f83c7e063a61961d154635f0c42
Vencislav Atanasov <user890104@freemyipod.org>
2026-08-31 17:43:33 +0300

    S5L8730: Fix UART baud rate clock source and clock rate

--- drivers/serial/serial_s5p.c
+++ drivers/serial/serial_s5p.c
@@ -110,7 +110,7 @@ static void __maybe_unused s5p_serial_init(struct s5p_uart *uart)
 	writel(UCON_RX_IRQ_OR_POLLING | UCON_TX_IRQ_OR_POLLING |
 	       UCON_RX_ERR_IRQ_EN | UCON_TX_IRQ_LEVEL, &uart->ucon);
 
-#if IS_ENABLED(CONFIG_S5L8720) || IS_ENABLED(CONFIG_S5L8723) || IS_ENABLED(CONFIG_S5L8740)
+#if IS_ENABLED(CONFIG_S5L8720) || IS_ENABLED(CONFIG_S5L8723) || IS_ENABLED(CONFIG_S5L8730) || IS_ENABLED(CONFIG_S5L8740)
 	u32 val = readl(&uart->ucon);
 	writel(val | S5L_CLK_NCLK, &uart->ucon);
 #endif
